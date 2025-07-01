b1372e26b8a2a82eda9646d5f9034cddd6269f8a
Sottim <santosottim@gmail.com>
2025-07-01 16:18:00 +0530

    Include the AHS logo in the navbar, in the browser tagline, change the nav bar design.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,12 +2,17 @@
 import React from 'react';
 import './Navbar.css'; // Import a CSS file for Navbar styling
 import logo from './ashoka-logo.png';
+import ahsLogo from './AHSLOGO.png';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <div className='ashoka-logo-container'><img className="ashoka-logo" src={logo} alt="Ashoka University Logo"/>
-        <div className="navbar-brand">Kutum Lab WSI Viewer</div>
-        </div>
+      <div className='ashoka-logo-container'><img className="ashoka-logo" src={logo} alt="Ashoka University Logo" />
+        <div className="navbar-brand">Augumented Health Systems : Whole Slide Image Viewer</div>
+      </div>
+      <div className="navbar-logo-right">
+        <img src={ahsLogo} alt="AHS Logo" className="ahs-logo" style={{ height: '95px', width: 'auto', objectFit: 'contain' }} />
+      </div>
     </nav>
   );
 };
